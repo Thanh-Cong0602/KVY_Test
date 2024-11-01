1e6534146375be96f52e188a8caa4079ff8d1ac1
Nguyen Thanh Cong <93293449+Thanh-Cong0602@users.noreply.github.com>
2024-11-01 16:11:32 +0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import Navbar from '@/components/Navbar'
-import ProductPage from '@/pages/Product/ProductPage'
-import ShoppingCart from '@/pages/ShoppingCart/ShoppingCart'
+import { Spin } from 'antd'
+import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 
+const ProductPage = lazy(() => import('@/pages/Product/ProductPage'))
+const ShoppingCart = lazy(() => import('@/pages/ShoppingCart/ShoppingCart'))
+
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path='/' element={<ProductPage />} />
-        <Route path='/shopping-cart' element={<ShoppingCart />} />
-      </Routes>
+      <Suspense fallback={<Spin />}>
+        <Routes>
+          <Route path='/' element={<ProductPage />} />
+          <Route path='/shopping-cart' element={<ShoppingCart />} />
+        </Routes>
+      </Suspense>
+
       <ToastContainer newestOnTop={true} className='toast-position' position='top-center' />
     </>
   )
